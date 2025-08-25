8f6970658df960f33ed5ef4d3603588ed0d170cd
Bautista <bautistamoyano1606@gmail.com>
2025-08-25 09:34:12 -0300

    Creamos el entorno virtual instalamos el flask y creamos la base de datos en un archivo sqlite

--- Servidor/Clase Pasada/app.py
+++ Servidor/Clase Pasada/app.py
@@ -1,14 +1,34 @@
-from flask import Flask
+import sqlite3
+from flask import Flask, g, jsonify, request, url_for
+from math import ceil
+
+def dict_factory(cursor, row):
+  """Arma un diccionario con los valores de la fila."""
+  fields = [column[0] for column in cursor.description]
+  return {key: value for key, value in zip(fields, row)}
+
+def abrirConexion():
+   if 'db' not in g:
+      g.db = sqlite3.connect("sensores.sqlite")
+      g.db.row_factory = dict_factory
+   return g.db
+
+def cerrarConexion(e=None):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
 
 app = Flask(__name__)
+app.teardown_appcontext(cerrarConexion)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/api/test")
+def test():
+    return "funcionando!"
 
 @app.route("/api/sensor", methods=['POST'])
-def json_Date():
-    data =request.json
-    name =data.get("luxometro")
-    age =data.get("123")
-    return print(name, age, "ok")
+def sensor():
+    datos = request.json
+    nombre = datos["nombre"]
+    valor = datos["valor"]
+    print(f"nombre del sensor {nombre}, valor: {valor}")
+    return "OK"
